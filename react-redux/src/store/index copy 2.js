57931c46bac6f1bfd2986cb9da963dc7fe53a670
Harsha C G <harshacg@yahoo.co.in>
2021-09-28 21:43:43 +0530

    few more addition

---
+++ react-redux/src/store/index copy 2.js
@@ -0,0 +1,52 @@
+import { createSlice, configureStore } from "@reduxjs/toolkit";
+
+const initialCounterState = { counter: 0, showCounter: true };
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState:initialCounterState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.payload;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
+
+const initialAuthState = {isAuthenticated: false}
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: initialAuthState,
+  reducers: {
+    login(state){
+      state.isAuthenticated = true;
+    },
+    logout(state){
+      state.isAuthenticated = false;
+    }
+  }
+})
+
+// const store = configureStore({
+//     reducer: counterSlice.reducer
+// });
+
+const store = configureStore({
+  reducer: {counter:counterSlice.reducer, auth:authSlice.reducer}
+});
+
+//   reducer: {counter: counterSlice.reducer}
+
+export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
+
+export default store;
